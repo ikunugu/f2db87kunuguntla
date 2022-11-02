a5413dcc83340f76ffcdc33a0094737e13ad1f7d
ikunugu <indusri2022@gmail.com>
2022-11-01 22:41:14 -0500

    modified gridbuild.js and gridbuild.pug

--- routes/gridbuild.js
+++ routes/gridbuild.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   let query = req.query
   console.log(`rows ${query.rows}`)
   console.log(`cols ${query.cols}`)
-  res.render('index', { title: 'gridbuild', query: query });
+  res.render('gridbuild', { title: 'gridbuild', query: query });
 });
 
 module.exports = router;

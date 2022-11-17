a3a1835bdda38c0fc8ff6929304192eeb2855249
ikunugu <indusri2022@gmail.com>
2022-11-17 00:46:59 -0600

    modified files

--- controllers/flight.js
+++ controllers/flight.js
@@ -29,7 +29,7 @@ exports.flight_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"flight_type":"goat", "cost":12, "size":"large"}
     document.flightName = req.body.flightName;
     document.flightType = req.body.flightType;
     document.price = req.body.price;
@@ -108,3 +108,45 @@ exports.flight_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a flight.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.flight_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('flightcreate', { title: 'Flight Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle building the view for updating a flight.
+// query provides the id
+exports.flight_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Flight.findById(req.query.id)
+    res.render('flightupdate', { title: 'Flight Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle a delete one view with id from query
+exports.flight_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Flight.findById(req.query.id)
+    res.render('flightdelete', { title: 'Flight Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/flights.js
+++ routes/flights.js
@@ -18,8 +18,14 @@ router.get('flight/:id', flight_controlers.flight_detail);
 /* GET detail costume page */
 router.get('/detail', flight_controlers.flight_view_one_Page);
 
+/* GET create costume page */
+router.get('/create', flight_controlers.flight_create_Page);
 
+/* GET create update page */
+router.get('/update', flight_controlers.flight_update_Page);
 
+/* GET delete costume page */
+router.get('/delete', flight_controlers.flight_delete_Page);
 
 
 
